c33e9ae1f11bd4965080bdfe93b5b1ef220c74af
Sebastian <spolwind.sebastian@googlemail.com>
2020-04-18 14:10:27 +0200

    RobotMovement - MoveToCursor

--- PlantingRobot/Assets/Scripts/Robot/Movement.cs
+++ PlantingRobot/Assets/Scripts/Robot/Movement.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool rotateToMouse = true;   //TODO: make it possible to change the rotation mode
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Input.GetAxis("Vertical") * Time.deltaTime * speed, 0f, -Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+    void Update() {
+        RotateToCursor();
+
+        float moveSpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(transform.worldToLocalMatrix.MultiplyVector(transform.forward) * moveSpeed);
+
+        //transform.Translate(Input.GetAxis("Vertical") * Time.deltaTime * speed, 0f, -Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+    }
+
+    void RotateToCursor() {
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        //Apply Angle
+        transform.rotation = Quaternion.Euler(new Vector3(0f, -angle, 0f));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 }
