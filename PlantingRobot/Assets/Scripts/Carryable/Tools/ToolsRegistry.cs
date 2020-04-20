b8a9d8c55a429b1d14a39905ad00dccfaf8cfb38
Sebastian <spolwind.sebastian@googlemail.com>
2020-04-20 17:04:08 +0200

    SummonTools (if one is throws them away)

--- PlantingRobot/Assets/Scripts/Carryable/Tools/ToolsRegistry.cs
+++ PlantingRobot/Assets/Scripts/Carryable/Tools/ToolsRegistry.cs
@@ -6,7 +6,20 @@ public class ToolsRegistry : MonoBehaviour
 {
     public Transform toolParent;
 
+    private List<Tool> tools =  new List<Tool>();
     public Transform GetToolParent() {
         return toolParent;
+    }
+
+    public void RegisterTool(Tool t) {
+        tools.Add(t);
+    }
+
+    public void DeregisterTool(Tool t) {
+        tools.Remove(t);
+    }
+
+    public List<Tool> GetAllTools() {
+        return tools;
     }
 }
