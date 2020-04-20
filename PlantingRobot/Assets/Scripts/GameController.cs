c1cf966a3c4ca312ff1c24d2f424f8574e617ca6
Sebastian <spolwind.sebastian@googlemail.com>
2020-04-20 19:54:04 +0200

    Remove Chanting add BackgroundMusic.,

--- PlantingRobot/Assets/Scripts/GameController.cs
+++ PlantingRobot/Assets/Scripts/GameController.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    private AudioSource audioSource = null;
+    private bool music = true;
+
+    public void Start() {
+        audioSource = gameObject.GetComponent<AudioSource>();    
+    }
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape)) {
             Application.Quit();
         }
+
+        if (Input.GetKeyDown(KeyCode.M)) {
+            ToggleBackgroundMusic();
+        }
+    }
+
+    public void ToggleBackgroundMusic() {
+        if(music) {
+            StopSound();
+            music = false;
+        } else {
+            StartSound();
+            music = true;
+        }
+    }
+
+    public void StopSound() {
+        audioSource.Pause();
+    }
+
+    public void StartSound() {
+        audioSource.Play();
     }
 }
